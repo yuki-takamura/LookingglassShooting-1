47aab6ad15ed1516e7d46420449ac379efe5fc45
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2019-03-30 20:58:56 +0900

    プレイヤーの移動処理実装

---
+++ LookingGlassShooting/Assets/Scripts/Move.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField]
+    private float m_speed = 1.0f;
+
+    private string m_tag;
+
+    void Start()
+    {
+        m_tag = transform.tag;
+    }
+
+    void Update()
+    {
+        if (m_tag == "Player1")
+        {
+            MovePlayerOne();
+            return;
+        }
+
+        if (m_tag == "Player2")
+        {
+            MovePlayerSecond();
+        }
+    }
+
+    void MovePlayerOne()
+    {
+        var horizontal = 0.0f;
+        var vertical = 0.0f;
+
+        vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+
+        MoveGameObject(horizontal, vertical);
+    }
+
+    void MovePlayerSecond()
+    {
+        var horizontal = 0.0f;
+        var vertical = 0.0f;
+
+        vertical = Input.GetAxis("Vertical2");
+        horizontal = Input.GetAxis("Horizontal2");
+
+        MoveGameObject(horizontal, vertical);
+    }
+
+    void MoveGameObject(float horizontal, float vertical)
+    {
+        transform.position += new Vector3(horizontal, vertical, 0) * m_speed * Time.deltaTime;
+    }
+}
