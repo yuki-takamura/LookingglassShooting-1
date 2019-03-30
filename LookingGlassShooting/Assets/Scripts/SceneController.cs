41def4c94ecab3a37df473bb9da494af4c9cf1a7
m2wasabi <m2wasabi@gmail.com>
2019-03-30 22:18:38 +0900

    ざっくりシーン遷移

---
+++ LookingGlassShooting/Assets/Scripts/SceneController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using LooklingGlassShooting.Models;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+
+    private GlobalRegistory glovalRegistory;
+    void Start()
+    {
+        glovalRegistory = GetComponent<GlobalRegistory>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ShowTitle()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void ShowCharacterSelect()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void CharactersSelected(SeasonFormat player1Season, SeasonFormat player2Season)
+    {
+        glovalRegistory.SetSeasons(player1Season,player2Season);
+        ShowBattle();
+    }
+
+    private void ShowBattle()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
