4959c761df7b7efed776cc217c255ddd0ec7a75a
nneri <junk1ekuni@gmail.com>
2019-03-30 21:48:29 +0900

    パーティクルシステムをいれました

---
+++ LookingGlassShooting/Assets/Particle/Scripts/ParticleController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleController : MonoBehaviour
+{
+    [SerializeField]
+    ParticleSystem spring;
+    [SerializeField]
+    ParticleSystem summer;
+    [SerializeField]
+    ParticleSystem fall;
+    [SerializeField]
+    ParticleSystem winter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spring.Play();
+        fall.Play();
+        winter.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
