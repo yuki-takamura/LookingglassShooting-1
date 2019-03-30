0068e17fd9458fcafb1c0cd699a8e90b4d846bac
yuki-takamura <g1527047@st.t-kougei.ac.jp>
2019-03-31 07:44:04 +0900

    弾を撃つ処理を実装

---
+++ LookingGlassShooting/Assets/Scripts/BulletController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour
+{
+    private float m_speed;
+
+    /// <summary>
+    /// 単位は秒
+    /// </summary>
+    [SerializeField]
+    private float m_lifeTime = 3.0f;
+
+    private void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    public void Initialize(float speed)
+    {
+        m_speed = speed;
+    }
+
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, m_speed * Time.deltaTime));
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(m_lifeTime);
+
+        Destroy(gameObject);
+    }
+}
---
+++ LookingGlassShooting/Assets/Scripts/Shoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    [SerializeField]
+    private BulletController m_bullet = null;
+
+    private bool m_isPlayerOne = true;
+
+    [SerializeField]
+    private float m_bulletSpeed = 10.0f;
+
+    void Start()
+    {
+        if (transform.parent.tag == "Player2")
+        {
+            m_isPlayerOne = false;
+        }
+    }
+
+    void Update()
+    {
+        if (m_isPlayerOne)
+        {
+            //if (Input.GetButtonDown("Fire1"))
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                var bullet = Instantiate(m_bullet.gameObject, transform.position, Quaternion.identity);
+                bullet.GetComponent<BulletController>().Initialize(m_bulletSpeed);
+            }
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(m_bullet, transform.position, Quaternion.identity);
+            bullet.GetComponent<BulletController>().Initialize(m_bulletSpeed);
+            return;
+        }
+    }
+}
