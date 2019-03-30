54383ef4d998b8ff9e51d89e10d7a25291680532
nneri <junk1ekuni@gmail.com>
2019-03-30 23:53:13 +0900

    パーティクル設定調整など

--- LookingGlassShooting/Assets/Particle/Scripts/ParticleController.cs
+++ LookingGlassShooting/Assets/Particle/Scripts/ParticleController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Season
+{
+    spring,summer,fall,winter
+}
 public class ParticleController : MonoBehaviour
 {
     [SerializeField]
@@ -12,17 +16,40 @@ public class ParticleController : MonoBehaviour
     ParticleSystem fall;
     [SerializeField]
     ParticleSystem winter;
+
+    public Season own;
+    public Season enemy;
+    public int damage;
+    public float hSliderValue = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
-        spring.Play();
-        fall.Play();
-        winter.Play();
+        //spring.Play();
+        //fall.Play();
+        //winter.Play();
+        if (own == Season.spring || enemy == Season.spring)
+        {
+            spring.Play();
+        }
+        if (own == Season.summer || enemy == Season.summer)
+        {
+            summer.Play();
+        }
+        if (own == Season.fall || enemy == Season.fall)
+        {
+            fall.Play();
+        }
+        if (own == Season.winter || enemy == Season.winter)
+        {
+            winter.Play();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var emission = spring.emission;
+        emission.rateOverTime = hSliderValue;
+            //= 0.1f;
     }
 }
